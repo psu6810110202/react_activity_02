597b88bbc1d32b32917fa04364693268242fb214
psu6810110202 <6810110202@psu.ac.th>
2025-12-17 22:35:25 +0700

    feat: add api prefix and complete docker setup

---
+++ dist/app.module.js
@@ -0,0 +1,41 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AppModule = void 0;
+const common_1 = require("@nestjs/common");
+const typeorm_1 = require("@nestjs/typeorm");
+const app_controller_1 = require("./app.controller");
+const app_service_1 = require("./app.service");
+const book_category_module_1 = require("./book-category/book-category.module");
+const book_category_entity_1 = require("./book-category/entities/book-category.entity");
+const book_entity_1 = require("./book/entities/book.entity");
+const book_module_1 = require("./book/book.module");
+let AppModule = class AppModule {
+};
+exports.AppModule = AppModule;
+exports.AppModule = AppModule = __decorate([
+    (0, common_1.Module)({
+        imports: [
+            typeorm_1.TypeOrmModule.forRoot({
+                type: 'postgres',
+                host: 'localhost',
+                port: 5432,
+                username: 'admin',
+                password: 'password123',
+                database: 'bookstore_dev',
+                entities: [book_category_entity_1.BookCategory, book_entity_1.Book],
+                synchronize: true,
+            }),
+            book_category_module_1.BookCategoryModule,
+            book_module_1.BookModule,
+        ],
+        controllers: [app_controller_1.AppController],
+        providers: [app_service_1.AppService],
+    })
+], AppModule);
+//# sourceMappingURL=app.module.js.map
